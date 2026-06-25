412e0fc48243e5aa7cfe9fca691f671634ffbe34
AvaniAgarwal-23 <avaniagarwal2306@gmail.com>
2026-06-25 15:52:46 +0530

    Add: buy-and-sell-crypto - submission-2

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-2.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int minprice=prices[0];
+        int mxprofit=0;
+        for(int i=1;i<prices.length;i++){
+            int profit=prices[i]-minprice;
+            mxprofit=Math.max(mxprofit,profit);
+            minprice=Math.min(minprice,prices[i]);
+        }
+        return mxprofit;
+    }
+}
